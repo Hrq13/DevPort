53167bfc18494728e546b77f72fc79f25dc564b6
Henrique F. Miranda <h.miranda20@outlook.com>
2021-12-20 17:41:39 -0300

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,22 @@
 class PortfoliosController < ApplicationController
   def index
-      @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @params = params.require(:portfolio).permit(:title, :subtitle, :body)
+    @portfolio_item = Portfolio.new(@params)
+
+    respond_to do |format|
+        if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: "Portfolio item #{@portfolio_item.title} has been created successfully." }
+        else
+            format.html { render :new }
+        end
+    end
   end
 end
