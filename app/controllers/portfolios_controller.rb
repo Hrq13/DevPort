469c569f5a34c2c2aed41df1fe70efac91d8f594
Henrique F. Miranda <h.miranda20@outlook.com>
2022-02-01 19:42:40 -0300

    Implement update, show and edit funcionalities in portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [ :edit, :update, :show ]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -19,4 +21,27 @@ class PortfoliosController < ApplicationController
         end
     end
   end
+
+  def edit
+  end
+
+  def update
+    @params = params.require(:portfolio).permit(:title, :subtitle, :body)
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(@params)
+        format.html { redirect_to portfolio_path(@portfolio_item[:id]), notice: "#{@portfolio_item.title} was successfully updated." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+  end
+
+  private
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 end
