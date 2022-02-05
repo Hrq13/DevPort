3841f11bf14edc7aa65b61c62c4365e8c32eb72a
Henrique F. Miranda <h.miranda20@outlook.com>
2022-02-05 13:47:28 -0300

    Implemented custom scopes in Portfolio module model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,6 +5,14 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def ruby_on_rails
+    @ruby_on_rails_portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,10 @@ class Portfolio < ApplicationRecord
     friendly_id :title, use: :slugged
 
     validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+    def self.angular
+        where(subtitle: "Angular")
+    end
+
+    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby On Rails") }
 end
